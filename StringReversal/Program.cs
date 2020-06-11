8e66bd1bd82510ab6b7b709adc4098ff80f35480
Peter Hansen <phansen@brainshark.com>
2020-06-11 11:24:12 -0400

    initial commit

---
+++ StringReversal/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringReversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var randomString = "234242heytest123";
+
+            char[] orderedArray = randomString.ToCharArray();
+
+            var left = 0;
+            var right = orderedArray.Length - 1;
+
+            for (int i = 0; left < right; left++, right--)
+            {
+                var temp = orderedArray[left];
+                orderedArray[left] = orderedArray[right];
+                orderedArray[right] = temp;
+            }
+
+            Console.WriteLine(orderedArray);
+            Console.ReadLine();
+        }
+    }
+}
